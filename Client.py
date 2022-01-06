eb2f9e37ef6ceae1057679fac3ab4925610b6f6f
eviatarbutin <eviatar2004@gmail.com>
2022-01-06 21:30:52 +0200

    working client need documentation

--- Client.py
+++ Client.py
@@ -4,26 +4,41 @@ import win32con
 import win32com
 import sys
 import os
-
-from Server import Server
-
+import threading
 
 BUFFER_SIZE = 2048
 IP = "127.0.0.1"
 PORT = 12345
-DIRECTORY_PATH=r"C:\Users\USER\Downloads"
+DIRECTORY_PATH="C:\\Users\\USER\\Downloads\\"
 FILE_LIST_DIRECTORY = 0x0001
-
+LOGFILE = "answers.txt"
 class Client:
-    def __init__(self, IP:str = IP, PORT:int = 12345, directory_path:str=DIRECTORY_PATH) -> None:
-        self.client_socket = socket.socket()
+    def __init__(self, IP:str = IP, PORT:int = PORT, directory_path:str=DIRECTORY_PATH) -> None:
         self.directory_path = directory_path
-        print(self.read_directoty_changes())
+        self.files_list = []
+        self.client_socket = socket.socket()
         self.client_socket.connect((IP,PORT))
-    
-    def send_file(self, file_name:str = "Books.exe"):
-        file = open(file_name,"r")
-        
+
+    # CHECK
+    def send_file(self):
+        while True:
+            if self.files_list:
+                file = open(DIRECTORY_PATH + self.files_list[0],"rb")
+                content = file.read()
+                file.seek(0)
+                packets = len(content)//BUFFER_SIZE + 1
+                self.client_socket.send(str(packets).encode())
+                if self.client_socket.recv(2) == "ok".encode():
+                    for i in range(packets):
+                        self.client_socket.send(file.read(BUFFER_SIZE))
+                    analysis = self.client_socket.recv(BUFFER_SIZE)
+                    with open(LOGFILE, "a") as logfile:
+                        logfile.write(f"{self.files_list[0]}: {analysis.decode()}\n")
+                    self.files_list.pop(0)
+                else:
+                    sys.stderr("didn't get an answer from the server")
+
+    # FIN
     def read_directoty_changes(self) -> str:
         hDir = win32file.CreateFile (
             self.directory_path,
@@ -34,7 +49,16 @@ class Client:
             win32con.FILE_FLAG_BACKUP_SEMANTICS,
             None
         )
-        return win32file.ReadDirectoryChangesW(hDir,BUFFER_SIZE,False,win32con.FILE_NOTIFY_CHANGE_LAST_WRITE,None,None)
+        while True:
+            change = win32file.ReadDirectoryChangesW(hDir,BUFFER_SIZE,False,win32con.FILE_NOTIFY_CHANGE_LAST_WRITE,None,None)[0]
+            print(change)
+            self.files_list.append(change[1])
 
+    def main(self):
+        directory_check = threading.Thread(target=self.read_directoty_changes)
+        client_send = threading.Thread(target=self.send_file)
+        directory_check.start()
+        client_send.start()
 if __name__ == "__main__":
     main = Client(IP,PORT,DIRECTORY_PATH)
+    main.main()
